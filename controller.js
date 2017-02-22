41c6c676ea410a70570ea8a448c452df2ffca591
Michael Gaudet <Michael.G224@gmail.com>
2017-02-22 13:20:27 -0600

    finished Mini-userProfile 2 project

--- controller.js
+++ controller.js
@@ -1,7 +1,9 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-
+    $scope.test = "still alive"
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	$scope.users = mainService.getUsers().then(function(response){
+        $scope.users=response.data.data
+    });
   }
 
 
